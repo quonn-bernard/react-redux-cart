648b91de98d3383521f2cb5426cf489765b33416
Quonn Bernard <quonnbernard@MacBook-Pro.local>
2020-08-31 10:58:32 -0700

    add redux to product filter

--- react-cart/src/actions/productActions.js
+++ react-cart/src/actions/productActions.js
@@ -1,11 +1,52 @@
-import { FETCH_PRODUCTS } from "../components/types";
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_PRICE,
+} from "../components/types";
 
-const fetchProducts = () => async(dispatch) => {
-    const result = await fetch('data/data.json');
-    const data = await result.json()
-    dispatch({
-        type: FETCH_PRODUCTS,
-        payload: data.products   
-    })
-}
-export default fetchProducts
+export const fetchProducts = () => async (dispatch) => {
+  const result = await fetch("data/data.json");
+  const data = await result.json();
+  dispatch({
+    type: FETCH_PRODUCTS,
+    payload: data.products,
+  });
+};
+export const filterProducts = (products, size) => (dispatch) => {
+  console.log("filtering");
+  dispatch({
+    type: FILTER_PRODUCTS_BY_SIZE,
+    payload: {
+      size: size,
+      items:
+        size === ""
+          ? products
+          : products.filter((item) => item.availableSizes.indexOf(size) >= 0),
+    },
+  });
+};
+
+export const sortProducts = (filteredProducts, sort) => (dispatch) => {
+  const sortedProducts = filteredProducts.slice();
+  if (sort === "latest") {
+    sortedProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
+  } else {
+    sortedProducts.sort((a, b) =>
+      sort === "lowest"
+        ? a.price > b.price
+          ? 1
+          : -1
+        : a.price > b.price
+        ? -1
+        : 1
+    );
+  }
+  console.log(sortedProducts);
+  dispatch({
+    type: ORDER_PRODUCTS_BY_PRICE,
+    payload: {
+      sort: sort,
+      items: sortedProducts,
+    },
+  });
+};
--- react-cart/src/reducers/productReducers.js
+++ react-cart/src/reducers/productReducers.js
@@ -1,11 +1,26 @@
-const { FETCH_PRODUCTS } = require("../components/types");
-const data = require('../data.json')
-const productsReducer = (state = {}, action) =>{
-    switch(action.type){
-        case FETCH_PRODUCTS:
-            return {items: action.payload}
-            default:
-                return state;
-    }
-}
+import { ORDER_PRODUCTS_BY_PRICE, FILTER_PRODUCTS_BY_SIZE, FETCH_PRODUCTS } from "../components/types";
+
+const data = require("../data.json");
+const productsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FILTER_PRODUCTS_BY_SIZE:
+      return {
+        ...state,
+        size: action.payload.size,
+        filteredItems: action.payload.items,
+      };
+
+    case ORDER_PRODUCTS_BY_PRICE:
+      console.log("sorting by price");
+      return {
+        ...state,
+        sort: action.payload.sort,
+        filteredItems: action.payload.items,
+      };
+    case FETCH_PRODUCTS:
+      return { items: action.payload, filteredItems: action.payload };
+    default:
+      return state;
+  }
+};
 export default productsReducer;
